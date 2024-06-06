4b7c02690d29e2b826a14092bfa3e79927805fe7
Ole-Marius Mathiassen <olemarius@gmail.com>
2024-06-06 11:01:05 +0200

    ui: add styling

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,6 +2,7 @@
 import React, { MouseEventHandler } from "react";
 import style from "./button.module.css";
 import "../../styles/globals.css";
+import "../../styles/tokens.css";
 
 export type ButtonProps = {
   text?: string;
@@ -11,7 +12,7 @@ export type ButtonProps = {
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, ...props }) => {
   return (
-    <button className={`${style.button} ${style.primary}`} type="button" onClick={onClick} {...props}>
+    <button className={`${style.button} ${style.primary} ${style.large}`} type="button" onClick={onClick} {...props}>
       {text}
     </button>
   );
--- src/components/button/__docs__/Button.stories.tsx
+++ src/components/button/__docs__/Button.stories.tsx
@@ -16,6 +16,7 @@ export const Primary: Story = {
     onClick: () => console.log("Button"),
   },
 };
+
 export const Secondary: Story = {
   args: {
     text: "Button",
