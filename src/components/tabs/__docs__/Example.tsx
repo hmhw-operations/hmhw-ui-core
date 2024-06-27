23770c2e83830df0aff63298fe8cffbe0fdf7dea
Nicklas Pouey-Winger <nicklas.pouey-winger@hmhw.com>
2024-06-27 14:53:39 +0200

    wip

---
+++ src/components/tabs/__docs__/Example.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Tabs from "../Tabs";
+
+// const tabItems: TabItem[] = [
+//   { label: "Tab 1", id: "tab1", content: "Tab 1 content" },
+//   { label: "Tab 2", id: "tab2", content: "Tab 2 content" },
+//   { label: "Tab 3", id: "tab3", content: "Tab 3 content" },
+// ];
+
+const Example: React.FC = () => {
+  return (
+    <>
+      <Tabs />
+    </>
+  );
+};
+
+export default Example;
---
+++ src/components/tabs/__docs__/Tabs.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Example from "./Example";
+
+const meta: Meta<typeof Example> = {
+  title: "Tabs",
+  component: Example,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Example>;
+
+export const Default: Story = {
+  //   args: {
+  //     disabled: false,
+  //   },
+};
