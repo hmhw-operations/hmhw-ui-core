3ebfdfac32956f45e0942dc7c46d045f4528a35a
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2024-07-03 12:19:30 +0200

    chore: template component (#12)
    Co-authored-by: Nicklas Pouey-Winger <nicklas.pouey-winger@hmhw.com>

---
+++ src/__template__/boilerplate/Boilerplate.tsx
@@ -0,0 +1,9 @@
+import "../../styles/globals.css";
+import "../../styles/palette.css";
+import "../../styles/tokens.css";
+
+const Boilerplate = () => {
+  return <div>Code here</div>;
+};
+
+export default Boilerplate;
