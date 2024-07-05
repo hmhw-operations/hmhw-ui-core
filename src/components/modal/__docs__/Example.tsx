33ea0ffb45d593ea464a41ffa71138e3f94b9d08
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2024-07-05 13:31:40 +0200

    feat: modal (#23)
    Co-authored-by: Nicklas Pouey-Winger <nicklas.pouey-winger@hmhw.com>

---
+++ src/components/modal/__docs__/Example.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Modal, { ActionType } from "../Modal";
+
+const mockActions: ActionType[] = [
+  { label: "Close", buttonVariant: "secondary", onClick: () => {} },
+  { label: "Save", buttonVariant: "primary", onClick: () => {} },
+];
+
+const Example = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <button onClick={() => setOpen(true)}>Click to open</button>
+      <Modal open={open} onClose={() => setOpen(false)} actions={mockActions} title={"Prop title"}>
+        Here is the content
+      </Modal>
+    </div>
+  );
+};
+
+export default Example;
