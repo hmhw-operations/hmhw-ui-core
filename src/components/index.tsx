6531983d7b6ea128da49d244c230940b2d94a8d1
Pouey-Winger <nicklas.pouey-winger@hmhw.com>
2024-08-15 13:53:42 +0200

    fix: add export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,12 @@
 // components/index.ts
 export * from "./button"; // Add more exports for other components as needed
 export * from "./accordion";
+export * from "./checkbox";
+export * from "./icon";
+export * from "./iconbutton";
+export * from "./modal";
+export * from "./radiobutton";
+export * from "./radiogroup";
+export * from "./segmentedbutton";
+export * from "./switch";
+export * from "./tabs";
