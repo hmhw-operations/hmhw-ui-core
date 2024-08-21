becfe52b694653f5bcb9c047b5475d827c8551ab
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2024-08-21 12:09:07 +0200

    feat: icon sizes (#29)

--- src/components/icon/__docs__/Icon.stories.tsx
+++ src/components/icon/__docs__/Icon.stories.tsx
@@ -4,9 +4,20 @@ import Example from "./Example";
 const meta: Meta<typeof Example> = {
   title: "Icon",
   component: Example,
+  argTypes: {
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Example>;
 
-export const Default: Story = {};
+export const Icon: Story = {
+  args: {
+    name: "day",
+    size: "medium",
+  },
+};
