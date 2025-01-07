bbd328808899fd51584ec2d3e624a7d37982b865
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2025-01-07 10:26:37 +0100

    fix: remove fixed width of 100%

--- src/components/navbar/__docs__/Example.tsx
+++ src/components/navbar/__docs__/Example.tsx
@@ -3,26 +3,16 @@ import Navbar from "../Navbar";
 
 const Example = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <Navbar
-        title="Automator"
-        links={[
-          { href: "#", label: "Home" },
-          { href: "#", label: "Users" },
-          { href: "#", label: "About" },
-        ]}
-        onThemeChange={() => console.log("Theme changed")}
-        logoutUrl="#"
-      />
-    </div>
+    <Navbar
+      title="Automator"
+      links={[
+        { href: "#", label: "Home" },
+        { href: "#", label: "Users" },
+        { href: "#", label: "About" },
+      ]}
+      onThemeChange={() => console.log("Theme changed")}
+      logoutUrl="#"
+    />
   );
 };
 
