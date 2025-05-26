f6d6c14a020b162404c3fd92ba22bbc43f2b31e5
Hartveit, Mathias <Mathias.Hartveit@hmhw.com>
2025-05-26 12:43:09 +0200

    feat: Finished baseline sidepanel functionality

--- src/components/sidepanel/__docs__/SidePanel.stories.tsx
+++ src/components/sidepanel/__docs__/SidePanel.stories.tsx
@@ -1,8 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import SidePanel from "../SidePanel";
+import Input from "../../input/Input";
 
-const meta: Meta<typeof SidePanel> = { title: "SidePanel", component: SidePanel };
+const meta: Meta<typeof SidePanel> = {
+  title: "SidePanel",
+  component: SidePanel,
+};
 
 export default meta;
 type Story = StoryObj<typeof SidePanel>;
@@ -15,8 +19,23 @@ export const Default: Story = {
     size: "large",
     titleComponent: <div>Panel Title</div>,
     actions: [
-      { label: "Action 1", buttonVariant: "primary", onClick: () => console.log("Action 1 clicked") },
-      { label: "Action 2", buttonVariant: "secondary", onClick: () => console.log("Action 2 clicked") },
+      {
+        label: "Action 1",
+        buttonVariant: "primary",
+        onClick: () => console.log("Action 1 clicked"),
+      },
+      {
+        label: "Action 2",
+        buttonVariant: "secondary",
+        onClick: () => console.log("Action 2 clicked"),
+      },
     ],
+    children: (
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <p>This is the content of the side panel.</p>
+        <p>You can add any React components here.</p>
+        <Input id="1234" title="Test" value={"0"} />
+      </div>
+    ),
   },
 };
