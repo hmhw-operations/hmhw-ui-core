32a4581a67cda29b6082f7884e1f00b62d6db2b6
Nicklas Pouey-Winger <nicklas.pouey-winger@hmhw.com>
2025-06-12 09:43:28 +0200

    WIP

--- src/components/select/__docs__/Select.stories.tsx
+++ src/components/select/__docs__/Select.stories.tsx
@@ -68,6 +68,46 @@ export const WithOptionGroups: Story = {
   }
 };
 
+export const ConnectorTypes: Story = {
+  args: {
+    label: "Choose Connector type",
+    optionGroups: [
+      {
+        label: "OPCUA",
+        options: [
+          { value: "opcua-server", text: "OpcUa Server Connector" },
+          { value: "opcua-client", text: "OpcUa Client Connector" }
+        ]
+      },
+      {
+        label: "UDP",
+        options: [{ value: "udp-server", text: "UDP Server Connector" }]
+      },
+      {
+        label: "TEST",
+        options: [
+          { value: "test", text: "Test Connector" },
+          { value: "test-datapoints", text: "Test Datapoints Reciever" },
+          { value: "test-reciever", text: "Test Reciever Connector" },
+          { value: "test-sender", text: "Test Sender Connector" }
+        ]
+      },
+      {
+        label: "STORAGE",
+        options: [{ value: "osisoft-pi", text: "Osisoft PI Storage Connector" }]
+      },
+      {
+        label: "CLOUD",
+        options: [
+          { value: "cloud-sync", text: "CloudSyncMetric Connector" },
+          { value: "signal-r", text: "SignalRCloud Connector" }
+        ]
+      }
+    ],
+    size: "medium"
+  }
+};
+
 export const Primary: Story = {
   name: "Custom placeholder",
   ...Select,
