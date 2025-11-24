834670d3446c80fd576499f0eb4da617841c2e11
Ole-Marius Mathiassen <olemarius@gmail.com>
2025-11-24 14:04:04 +0100

    New version of accordion (#170)
    Co-authored-by: Ole-Marius <ole-marius.mathiassen@hmhw.com>

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,9 +1,6 @@
 // components/accordion/Accordion.tsx
 import style from "./accordion.module.css";
-import "../../styles/globals.css";
-import "../../styles/palette.css";
-import "../../styles/tokens.css";
-import "../../styles/variables.css";
+import Icon from "../icon/Icon";
 import React from "react";
 import { Size } from "../../types";
 
@@ -19,11 +16,9 @@ export type AccordionProps = {
 
 const Accordion: React.FC<AccordionProps> = ({ items, size }) => {
   return items.map((item, index) => (
-    <details key={item.label + index} className={`${style.accordion} ${style[size ?? "medium"]}`}>
-      <summary>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path d="M11.2341 16.5873L14.6179 12.9779C15.1274 12.4344 15.1274 11.5564 14.6179 11.0129L11.2341 7.40354C10.411 6.53952 9 7.1527 9 8.39299V15.5979C9 16.8521 10.411 17.4653 11.2341 16.5873Z" />
-        </svg>
+    <details name="tech-specs" key={item.label + index} className={`${style.accordion} ${style[size ?? "medium"]}`}>
+      <summary className={`${style.header} `}>
+        <Icon name={"caret-right"} size={size ?? "small"} />
         {item.label}
       </summary>
       <div className={style.content}>{item.content}</div>
