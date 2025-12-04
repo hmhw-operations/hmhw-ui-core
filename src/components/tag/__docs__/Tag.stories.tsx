8d5e0ecc70539abf5fe5748ed7c6633dc8d4bb63
Ole-Marius <olemarius@gmail.com>
2025-12-04 11:41:13 +0100

    Fix bug in Tag component

--- src/components/tag/__docs__/Tag.stories.tsx
+++ src/components/tag/__docs__/Tag.stories.tsx
@@ -1,12 +1,27 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Example from "./Example";
+import { Meta, StoryObj } from "@storybook/react";
+import Tag from "../Tag";
 
-const meta: Meta<typeof Example> = {
+const meta: Meta<typeof Tag> = {
+  component: Tag,
   title: "Tag",
-  component: Example
+  argTypes: {
+    text: { control: "text" },
+    size: { control: "select", options: ["x-small", "small", "medium", "large", "x-large"] },
+    type: { control: "select", options: ["neutral", "accent", "info", "warning", "success", "danger"] },
+    rounded: { control: "boolean" },
+    weak: { control: "boolean" }
+  }
 };
 
 export default meta;
-type Story = StoryObj<typeof Example>;
+type Story = StoryObj<typeof Tag>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    text: "Tag",
+    size: "medium",
+    type: "neutral",
+    rounded: false,
+    weak: false
+  }
+};
