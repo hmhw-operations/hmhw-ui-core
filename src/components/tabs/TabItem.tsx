84f923209c12e0c475f86db8f182e11aaf8e1c14
Ole-Marius <olemarius@gmail.com>
2025-12-09 16:19:11 +0100

    WIP

--- src/components/tabs/TabItem.tsx
+++ src/components/tabs/TabItem.tsx
@@ -2,10 +2,6 @@ import { TabItemProps } from "./types";
 import { sanitizeForId } from "../../utils";
 import styles from "./tabs.module.css";
 
-import "../../styles/tokens.css";
-import "../../styles/globals.css";
-import "../../styles/variables.css";
-
 const TabItem: React.FC<TabItemProps> = ({ label, children }) => (
   <div className={styles.tab_panel} role="tabpanel" aria-labelledby={`tab-${sanitizeForId(label)}`} id={`panel-${sanitizeForId(label)}`}>
     {children}
