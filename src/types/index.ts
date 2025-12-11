dc8362ac1b6fd6c08948d7b9adf644effe895e84
Ole-Marius <olemarius@gmail.com>
2025-12-11 14:01:51 +0100

    Add file to FieldType

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export type Size = "x-small" | "small" | "medium" | "large" | "x-large";
 
 export type LabelPosition = "left" | "right" | "top" | "none";
 
-export type FieldType = "number" | "text" | "password" | "email" | "tel" | "url";
+export type FieldType = "number" | "text" | "password" | "email" | "tel" | "url" | "file";
 
 export type BaseComponentProps = {
   id: string;
