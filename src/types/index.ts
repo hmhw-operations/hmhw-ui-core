cde0ea16dc6841ece9909a18ec0d62a12c6d95a0
Ole-Marius Mathiassen <olemarius@gmail.com>
2025-12-12 08:59:45 +0100

    Add file upload to input (#179)
    * Add file to FieldType
    
    * Wip
    
    * Fix button

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,9 @@ export type Size = "x-small" | "small" | "medium" | "large" | "x-large";
 
 export type LabelPosition = "left" | "right" | "top" | "none";
 
-export type FieldType = "number" | "text" | "password" | "email" | "tel" | "url";
+export type FieldType = "number" | "text" | "password" | "email" | "tel" | "url" | "file";
+
+export type InputType = "text" | "email" | "password" | "number" | "tel" | "url" | "date" | "time" | "file";
 
 export type BaseComponentProps = {
   id: string;
